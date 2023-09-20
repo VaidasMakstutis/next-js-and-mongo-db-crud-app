7424b9fe90c1946da19a085f0219935b122a47d8
Vaidas <vaidas.makstutis@gmail.com>
2023-09-20 17:59:23 +0300

    success message component and toggle form

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,26 @@
 "use client";
 import { BiUserPlus } from "react-icons/bi";
+import { useState } from "react";
 import Table from "./components/Table";
 import Form from "./components/Form";
 
 const Home = () => {
+  const [visible, setVisible] = useState(false);
+
+  const handleAddEmployee = () => {
+    setVisible(!visible);
+  };
+
   return (
     <section>
       <main className="py-5">
         <h1 className="text-xl md:text-5xl text-center font-bold py-10">Employee Management</h1>
         <div className="container mx-auto flex-justify-between py-5 border-b">
           <div className="left flex gap-3">
-            <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-gray-800">
+            <button
+              onClick={handleAddEmployee}
+              className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-gray-800"
+            >
               Add Employee{" "}
               <span className="px-1">
                 <BiUserPlus size={23}></BiUserPlus>
@@ -19,9 +29,7 @@ const Home = () => {
           </div>
         </div>
         {/*collapsable form */}
-        <div className="container mx-auto py-5">
-          <Form />
-        </div>
+        {visible ? <Form /> : <></>}
         {/*table */}
         <div className="container mx-auto">
           <Table />

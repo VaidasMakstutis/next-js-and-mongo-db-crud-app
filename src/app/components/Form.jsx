16bb6bcd6bf8901d5b9bb78dfb85d9a620a53e30
Vaidas <vaidas.makstutis@gmail.com>
2023-09-20 15:45:21 +0300

    success and not success messages

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { useReducer } from "react";
 import { BiPlus } from "react-icons/bi";
+import Success from "./Success";
+import NotSuccess from "./NotSuccess";
 
 const formReducer = (state, event) => {
   return {
@@ -14,8 +16,11 @@ const Form = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
+    if (Object.keys(formData).length == 0) return console.log("No Form Data!");
     console.log(formData);
   };
+
+  if (Object.keys(formData).length > 0) return <NotSuccess message={"Data Added"} />;
 
   return (
     <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>

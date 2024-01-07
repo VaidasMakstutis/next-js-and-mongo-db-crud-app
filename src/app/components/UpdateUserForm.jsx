c5fbad0392e3534a425db0dbd13526e726a6d417
Vaidas <vaidas.makstutis@gmail.com>
2024-01-07 21:34:09 +0200

    update data in record

--- src/app/components/UpdateUserForm.jsx
+++ src/app/components/UpdateUserForm.jsx
@@ -1,10 +1,17 @@
 "use client";
 import { BiBrush } from "react-icons/bi";
-import { useQuery } from "react-query";
-import { getUser } from "../../../lib/helper";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import { getUser, getUsers, updateUser } from "../../../lib/helper";
 
 const UpdateUserForm = ({ formId, formData, setFormData }) => {
+  const queryClient = useQueryClient();
   const { isLoading, isError, data, error } = useQuery(["users", formId], () => getUser(formId));
+  const updateMutation = useMutation(newData => updateUser(formId, newData), {
+    onSuccess: async data => {
+      // queryClient.setQueryData('users', (old) => [data])
+      queryClient.prefetchQuery("users", getUsers);
+    }
+  });
 
   if (isLoading) return <h2>Loading...!</h2>;
   if (isError) return <h2>Error</h2>;
@@ -12,10 +19,11 @@ const UpdateUserForm = ({ formId, formData, setFormData }) => {
   const { name, avatar, salary, date, email, status } = data;
   const [firstname, lastname] = name ? name.split(" ") : formData;
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    if (Object.keys(formData).length == 0) return console.log("No Form Data!");
-    console.log(formData);
+    let userName = `${formData.firstname ?? firstname} ${formData.lastname ?? lastname}`;
+    let updated = Object.assign({}, data, formData, { name: userName });
+    await updateMutation.mutate(updated);
   };
 
   return (
